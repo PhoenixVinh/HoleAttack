2398b8f4733e33cd343efbadd27b33c2959d6bc5
luanGD-cyber <phonenix.luanvu@gmail.com>
2025-05-05 18:08:35 +0700

    edit level
    edit level

--- Assets/_Scripts/Editor/Ultils/ChangeSizeItemEditor.cs
+++ Assets/_Scripts/Editor/Ultils/ChangeSizeItemEditor.cs
@@ -35,6 +35,7 @@ namespace _Scripts.Editor.Ultils
 
         public bool ShowEditorSize = false;
         public List<Scale> Size;
+        private Vector3 scrollPosition = new Vector3(0, 0, 0);
         public void OnGUI()
         {
             
@@ -82,6 +83,8 @@ namespace _Scripts.Editor.Ultils
                 //GUILayout.BeginVertical();
                 
                 int index = 0;
+                scrollPosition = GUILayout.BeginScrollView(scrollPosition);
+                EditorGUI.BeginChangeCheck();
                 foreach (var item in itemDatas)
                 {
                 
@@ -95,7 +98,12 @@ namespace _Scripts.Editor.Ultils
                     GUILayout.EndHorizontal();
                     index++;
                 }
-                ShowItemAgain();
+                GUILayout.EndScrollView();
+                if (EditorGUI.EndChangeCheck())
+                {
+                    ShowItemAgain();
+                }
+               
 
      
             }
